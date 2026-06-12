480850734424828cfbccbef325082e2ebe711388
Avinash Jha <avinashjha@Avinashs-MacBook-Air-2.local>
2026-06-12 15:56:46 +0530

    updated backend leaderboard route to sort by score and then by last submission time

--- routes/leaderboardRoutes.js
+++ routes/leaderboardRoutes.js
@@ -1,13 +1,18 @@
 const express = require("express");
 
 const protect = require("../middleware/authMiddleware");
+const trainerOnly = require("../middleware/roleMiddleware");
 
 const {
   getLeaderboard,
+  syncAllLeaderboard,
 } = require("../controllers/leaderboardController");
 
 const router = express.Router();
 
 router.get("/", protect, getLeaderboard);
+router.post("/sync-all", protect, trainerOnly, syncAllLeaderboard);
 
 module.exports = router;
+
+
